3828aff300d07673e958f8c20d32ccbc93e399f4
= <=>
2023-11-13 17:26:29 -0800

    Just added some file related logging features to the logger

--- include/Logger/core/Logger.h
+++ include/Logger/core/Logger.h
@@ -9,37 +9,37 @@ namespace Logger{
     template<typename... T>
     void warn(std::string fmt, T&&... args){
         auto current = std::chrono::system_clock::now();
-        formatCpp::print(formatCpp::format("{}WARN[{:%c}]:{} {}", Colors::Yellow, current, Colors::Reset, fmt, (args)...));
+        formatCpp::print(formatCpp::format("{}WARN[{:%c}]: {} {}", Colors::Yellow, current, Colors::Reset, fmt, (args)...));
     }
 
     template<typename... T>
     void info(std::string fmt, T&&... args){
         auto current = std::chrono::system_clock::now();
-        formatCpp::print(formatCpp::format("{}INFO[{:%c}]:{} {}", Colors::Yellow, current, Colors::Reset, fmt), (args)...);
+        formatCpp::print(formatCpp::format("{}INFO[{:%c}]: {} {}", Colors::Yellow, current, Colors::Reset, fmt), (args)...);
     }
 
     template<typename... T>
     void debug(std::string fmt, T&&... args){
         auto current = std::chrono::system_clock::now();
-        formatCpp::print(formatCpp::format("{}DEBUG[{:%c}]:{} {}", Colors::Blue, current, Colors::Reset, fmt), (args)...);
+        formatCpp::print(formatCpp::format("{}DEBUG[{:%c}]: {} {}", Colors::Blue, current, Colors::Reset, fmt), (args)...);
     }
 
     template<typename... T>
     void trace(std::string fmt, T&&... args){
         auto current = std::chrono::system_clock::now();
-        formatCpp::print(formatCpp::format("{}TRACE[{:%c}]:{} {}", Colors::Blue, current, Colors::Reset, fmt), (args)...);
+        formatCpp::print(formatCpp::format("{}TRACE[{:%c}]: {} {}", Colors::Blue, current, Colors::Reset, fmt), (args)...);
     }
 
     template<typename... T>
     void error(std::string fmt, T&&... args){
         auto current = std::chrono::system_clock::now();
-        formatCpp::print(formatCpp::format("{}ERROR[{:%c}]:{} {}", Colors::Red, current, Colors::Reset, fmt), (args)...);
+        formatCpp::print(formatCpp::format("{}ERROR[{:%c}]: {} {}", Colors::Red, current, Colors::Reset, fmt), (args)...);
     }
 
     template<typename... T>
     void fatal(std::string fmt, T&&... args){
         auto current = std::chrono::system_clock::now();
-        formatCpp::print(formatCpp::format("{}FATAL[{:%c}]:{} {}", Colors::Red, current, Colors::Reset, fmt), (args)...);
+        formatCpp::print(formatCpp::format("{}FATAL[{:%c}]: {} {}", Colors::Red, current, Colors::Reset, fmt), (args)...);
     }
 
     template<typename... T>
@@ -80,7 +80,7 @@ namespace Logger{
             }
             auto current = std::chrono::system_clock::now();
             // formatCpp::print(formatCpp::format("{}WARN[{:%c}]:{} {}", Colors::Yellow, current, Colors::Reset, fmt, (args)...));
-            this->print(formatCpp::format("WARN[{:%c}]:{} {}", current, Colors::Reset, fmt, (args)...));
+            this->print(formatCpp::format("WARN[{:%c}]: {} {}", current, Colors::Reset, fmt, (args)...));
 
         }
 
